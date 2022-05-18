3f36a36f108e609a0f34e5194c75aacc4adf3e22
mujeeb <mujeeb.noorzad@gmail.com>
2022-05-17 17:48:53 -0700

    hey the issue fix

--- src/solar/Main.java
+++ src/solar/Main.java
@@ -1,17 +1,17 @@
 package solar;
 
 public class Main {
-    public static void main(String[] args)
-    {
-        solarSystem FactorX=new solarSystem();
-        FactorX.Sun="1";
-        FactorX.Moon="4";
-        FactorX.Planet="2";
-        FactorX.Star="10000";
+    public static void main(String[] args) {
+        solarSystem FactorX = new solarSystem();
+        FactorX.Sun = "1";
+        FactorX.Moon = "4";
+        FactorX.Planet = "2";
+        FactorX.Star = "10000";
 
 
-        FeatureSun Xfact=new FeatureSun();
-        Xfact.color="green";
-        Xfact.radius="1000000000km";
-        Xfact.heat="309324934898kj";
+        FeatureSun Xfact = new FeatureSun();
+        Xfact.color = "green";
+        Xfact.radius = "1000000000km";
+        Xfact.heat = "309324934898kj";
+    }
 }
